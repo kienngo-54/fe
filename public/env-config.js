f49b82773e9075b7281f58101b711eba40882371
kienngo-54 <kien.ngo2450@hcmut.edu.vn>
2024-10-04 21:59:49 +0700

    update link

--- public/env-config.js
+++ public/env-config.js
@@ -1,1 +1,1 @@
-window._env_ = {"PORT":"4001","API_URL":"https://sportvenue.onrender.com/api","CMS_URL":"http://localhost:4002/"}
+window._env_ = {"PORT":"4001","API_URL":"https://sportvenue.onrender.com/api","CMS_URL":"https://sport-venue-cms.vercel.app"}
